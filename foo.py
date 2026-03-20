0461449ac774e293cb6d63209cff37825fe26e42
jl404-ljj <JL404@student.london.ac.uk>
2026-03-20 17:05:43 +0800

    correct bugs in foo.py

--- foo.py
+++ foo.py
@@ -9,12 +9,13 @@ def is_divisible_by_k(x,k):
 Store all the integers that are multiples of 2 or 5 or 7 that are lower or equal to 1000 (excluding doubles)
 '''
 
-x = ()
-for i in range(1000):
-    if (is_divisible_by_k(x,2)) & is_divisible_by_k(x,3)) |
-is_divisible_by_k(x,7):
-    x.append(i)
-    
+x = []
+for i in range(1,1001):
+    if (is_divisible_by_k(i,2)) or is_divisible_by_k(i,3) or is_divisible_by_k(i,7):
+        x.append(i)
+        
+
 '''
 Sum all the intergers that are multiples of 2 or 5 or 7 that are lower or equal to 1000(excluding doubles)
 '''
+total_sum = sum(x)
